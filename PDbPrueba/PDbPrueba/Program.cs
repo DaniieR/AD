c99c38d271c887f2bcd6140d429b2091cf674dec
DaniieR <dam.dromero@institutoserpis.org>
2016-09-15 11:20:43 +0200

    Creado menu

--- PDbPrueba/PDbPrueba/Program.cs
+++ PDbPrueba/PDbPrueba/Program.cs
@@ -7,12 +7,22 @@ namespace PDbPrueba
 	{
 		public static void Main (string[] args)
 		{
-			Console.WriteLine ("Hello World!");
+			Console.WriteLine ("Probando Dbprueba");
+			Console.WriteLine ("Seleccione una opcion");
+			Console.WriteLine ("1 Nuevo");
+			Console.WriteLine ("2 Editar");
+			Console.WriteLine ("3 Eliminar");
+			Console.WriteLine ("4 Listar Todos");
+			Console.WriteLine ("0 Salir");
 
 			MySqlConnection mySqlConnection = new MySqlConnection ("Database=dbprueba;User Id=root;Password=sistemas");
 			mySqlConnection.Open ();
 			//Operaciones...
-			mySqlConnection.Close ();
+			switch (Console.Read ()) {
+			case '0':
+				mySqlConnection.Close ();
+			break;
+			};
 		}
 	}
 }
