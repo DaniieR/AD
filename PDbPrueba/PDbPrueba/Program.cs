c8484aa338583d830a94a05b3c80786ad33f5b5c
DaniieR <dam.dromero@institutoserpis.org>
2016-09-15 10:02:51 +0200

    Añadido PDbPrueba conectando a dbprueba

---
+++ PDbPrueba/PDbPrueba/Program.cs
@@ -0,0 +1,18 @@
+using System;
+using MySql.Data.MySqlClient;
+
+namespace PDbPrueba
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Hello World!");
+
+			MySqlConnection mySqlConnection = new MySqlConnection ("Database=dbprueba;User Id=root;Password=sistemas");
+			mySqlConnection.Open ();
+			//Operaciones...
+			mySqlConnection.Close ();
+		}
+	}
+}
