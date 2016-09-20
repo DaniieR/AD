99bf72de8576f4a055847dc5b63011fd96a440a4
DaniieR <dam.dromero@institutoserpis.org>
2016-09-20 10:45:32 +0200

    Añadido los metodos de PDbPrueba

--- PDbPrueba/PDbPrueba/Program.cs
+++ PDbPrueba/PDbPrueba/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Data;
 using MySql.Data.MySqlClient;
 
 namespace PDbPrueba
@@ -7,22 +8,75 @@ namespace PDbPrueba
 	{
 		public static void Main (string[] args)
 		{
-			Console.WriteLine ("Probando Dbprueba");
-			Console.WriteLine ("Seleccione una opcion");
-			Console.WriteLine ("1 Nuevo");
-			Console.WriteLine ("2 Editar");
-			Console.WriteLine ("3 Eliminar");
-			Console.WriteLine ("4 Listar Todos");
-			Console.WriteLine ("0 Salir");
-
-			MySqlConnection mySqlConnection = new MySqlConnection ("Database=dbprueba;User Id=root;Password=sistemas");
-			mySqlConnection.Open ();
+			IDbConnection dbConnection = new MySqlConnection ("Database=dbprueba;User Id=root;Password=sistemas");
+			dbConnection.Open ();
 			//Operaciones...
-			switch (Console.Read ()) {
-			case '0':
-				mySqlConnection.Close ();
+			IDbCommand dbCommand = dbConnection.CreateCommand ();
+			IDataReader dataReader; 
+			String cat;
+			do{
+				Console.WriteLine ("Probando Dbprueba");
+				Console.WriteLine ("------------------------");
+				Console.WriteLine ("Seleccione una opcion");
+				Console.WriteLine ("------------------------");
+				Console.WriteLine ("1 Nuevo");
+				Console.WriteLine ("2 Editar");
+				Console.WriteLine ("3 Eliminar");
+				Console.WriteLine ("4 Listar Todos");
+				Console.WriteLine ("0 Salir");
+				int opcion = Console.Read ();
+			switch (opcion) {
+			case '1':
+				dbCommand.CommandText = "insert into categoria (nombre) values (@nombre)";
+				IDbDataParameter dbDataParameter = dbCommand.CreateParameter ();
+				dbDataParameter.ParameterName = "nombre";
+				Console.WriteLine ("Introduzca una categoria");
+				cat = Console.ReadLine ();
+				dbDataParameter.Value = cat;
+				dbCommand.Parameters.Add (dbDataParameter);
+				dbCommand.ExecuteNonQuery ();
 			break;
+			case '2':
+					dbCommand.CommandText = "select * from categoria";
+					dataReader = dbCommand.ExecuteReader ();
+					Console.WriteLine ("");
+					Console.WriteLine ("-----------------------");
+					while (dataReader.Read()) {
+
+						int id = dataReader.GetInt32 (0);
+						String nombre = dataReader.GetString (1);
+						Console.WriteLine ("ID " + id + " " + "Nombre " + nombre);
+					}
+					dataReader.Close();
+					Console.WriteLine ("Introduzca una categoria");
+					String catmod = Console.ReadLine ();
+					Console.WriteLine ("Introduzca el ID");
+					String idmod = Console.ReadLine ();
+					int idint = int.Parse (idmod);
+
+
+			break;
+			case '4':
+
+				dbCommand.CommandText = "select * from categoria";
+				dataReader = dbCommand.ExecuteReader ();
+				Console.WriteLine ("");
+				Console.WriteLine ("-----------------------");
+				while (dataReader.Read()) {
+
+					int id = dataReader.GetInt32 (0);
+					String nombre = dataReader.GetString (1);
+					Console.WriteLine ("ID " + id + " " + "Nombre " + nombre);
+				}
+				dataReader.Close();
+				break;
+			case '0':
+				dbConnection.Close ();
+				dbConnection=null;
+				break;
+				
 			};
+			}while(dbConnection!=null);
 		}
 	}
 }
