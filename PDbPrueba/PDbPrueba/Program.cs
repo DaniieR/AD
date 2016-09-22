692eaf080a9be472623f0128a4ae327f1059b1f2
luisvelasco16 <luis.velasco@institutoserpis.org>
2016-09-22 09:10:05 +0200

    En PDbPrueba añadidos métodos de ejemplo para insert y select

--- PDbPrueba/PDbPrueba/Program.cs
+++ PDbPrueba/PDbPrueba/Program.cs
@@ -1,5 +1,6 @@
-using System;
 using MySql.Data.MySqlClient;
+using System;
+using System.Data;
 
 namespace PDbPrueba
 {
@@ -7,19 +8,34 @@ namespace PDbPrueba
 	{
 		public static void Main (string[] args)
 		{
-//			Console.WriteLine ("Probando dbprueba");
-//
-//			MySqlConnection mySqlConnection = new MySqlConnection (
-//				"Database=dbprueba;User Id=root;Password=sistemas"
-//			);
-//
-//			mySqlConnection.Open ();
-//
-//			//operaciones...
-//
-//			mySqlConnection.Close ();
+			Console.WriteLine ("Probando dbprueba");
 
-			readLong ("Introduce el id: ");
+			IDbConnection dbConnection = new MySqlConnection (
+				"Database=dbprueba;User Id=root;Password=sistemas"
+			);
+
+			dbConnection.Open ();
+
+			//operaciones...
+//			IDbCommand dbCommand = dbConnection.CreateCommand ();
+//			dbCommand.CommandText = "insert into categoria (nombre) values (@nombre)";
+//			IDbDataParameter dbDataParameter = dbCommand.CreateParameter ();
+//			dbDataParameter.ParameterName = "nombre";
+//			dbDataParameter.Value = "categoría 4";
+//			dbCommand.Parameters.Add (dbDataParameter);
+//			dbCommand.ExecuteNonQuery ();
+
+			//para select...
+			IDbCommand dbCommand = dbConnection.CreateCommand ();
+			dbCommand.CommandText = "select * from categoria";
+			IDataReader dataReader = dbCommand.ExecuteReader ();
+			while (dataReader.Read()) 
+				Console.WriteLine ("{0,4} {1}", dataReader ["id"], dataReader ["nombre"]);
+			dataReader.Close ();
+
+			dbConnection.Close ();
+
+			//readLong ("Introduce el id: ");
 		}
 
 		private static long readLong(string label) {
