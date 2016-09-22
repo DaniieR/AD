93a5fab7dd8d175b56de54369a969361438f322d
luisvelasco16 <luis.velasco@institutoserpis.org>
2016-09-22 10:50:15 +0200

    Añadido PCategoria: resuelto getOption y ciclo principal

---
+++ PCategoria/PCategoria/Program.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Text.RegularExpressions;
+
+namespace PCategoria
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+
+			while (true) {
+				int option = getOption ();
+				switch (option) {
+				case 0:
+
+					return;
+				case 1:
+					nuevo ();
+					break;
+				case 2:
+					editar ();
+					break;
+				case 3:
+					borrar ();
+					break;
+				case 4:
+					listar ();
+					break;
+				}
+
+			}
+		}
+
+		private static void nuevo() {
+		}
+
+		private static void editar() {
+		}
+
+		private static void borrar() {
+		}
+
+		private static void listar() {
+		}
+
+		private static int getOption() {
+			string pattern = "^[01234]$";
+			while (true) {
+				Console.WriteLine ("0 Salir");
+				Console.WriteLine ("1 Nuevo");
+				Console.WriteLine ("2 Editar");
+				Console.WriteLine ("3 Borrar");
+				Console.WriteLine ("4 Listar");
+				string option = Console.ReadLine ();
+				if (Regex.IsMatch (option, pattern))
+					return int.Parse (option);
+				Console.WriteLine("Opción inválida. Vuelve a introducir.");
+			}
+		}
+	}
+}
