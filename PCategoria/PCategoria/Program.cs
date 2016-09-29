1aa8b4ef45120d13aa99ee5bad66651941dc25a3
luisvelasco16 <luis.velasco@institutoserpis.org>
2016-09-29 10:04:01 +0200

    PCategoria terminado incluyendo manejo básico de excepciones

--- PCategoria/PCategoria/Program.cs
+++ PCategoria/PCategoria/Program.cs
@@ -17,7 +17,6 @@ namespace PCategoria
 			dbConnection = new MySqlConnection (
 				"Database=dbprueba;User Id=root;Password=sistemas"
 			);
-			//TODO valorar si merece la pena una inicialización perezosa
 			dbConnection.Open ();
 			while (true) {
 				Option option = getOption ();
@@ -38,7 +37,6 @@ namespace PCategoria
 					listar ();
 					break;
 				}
-
 			}
 		}
 
@@ -48,14 +46,28 @@ namespace PCategoria
 			IDbCommand dbCommand = dbConnection.CreateCommand ();
 			dbCommand.CommandText = INSERT_SQL;
 			addParameter (dbCommand, "nombre", nombre);
-			dbCommand.ExecuteNonQuery ();
+			try {
+				dbCommand.ExecuteNonQuery ();
+			} catch (MySqlException ex) {
+				Console.WriteLine (getUserMessage(ex));				
+			}
 		}
 
 		private static string UPDATE_SQL = "update categoria set nombre=@nombre where id=@id";
 		private static void editar() {
 			long id = readLong ("Id: ");
 			string nombre = readString("Nombre: ");
-
+			IDbCommand dbCommand = dbConnection.CreateCommand ();
+			dbCommand.CommandText = UPDATE_SQL;
+			addParameter (dbCommand, "id", id);
+			addParameter (dbCommand, "nombre", nombre);
+			try {
+				int filas = dbCommand.ExecuteNonQuery (); //devolverá 0 o 1
+				if (filas == 0)
+					Console.WriteLine ("Id no existente. No existe ningún registro con ese Id.");
+			} catch (MySqlException ex) {
+				Console.WriteLine (getUserMessage(ex));				
+			}
 		}
 
 		private static string DELETE_SQL = "delete from categoria where id=@id";
@@ -64,11 +76,29 @@ namespace PCategoria
 			IDbCommand dbCommand = dbConnection.CreateCommand ();
 			dbCommand.CommandText = DELETE_SQL;
 			addParameter (dbCommand, "id", id);
-			//TODO comprobar si devuelve 0 (ningún registro afectado)...
-			dbCommand.ExecuteNonQuery ();
+			int filas = dbCommand.ExecuteNonQuery (); //devolverá 0 o 1
+			if (filas == 0)
+				Console.WriteLine ("Id no existente. No existe ningún registro con ese Id.");
 		}
 
+		private static string SELECT_SQL = "select * from categoria order by id";
 		private static void listar() {
+			IDbCommand dbCommand = dbConnection.CreateCommand ();
+			dbCommand.CommandText = SELECT_SQL;
+			IDataReader dataReader = dbCommand.ExecuteReader ();
+			Console.WriteLine("{0,5} {1}", "Id", "Nombre");
+			while (dataReader.Read()) 
+				Console.WriteLine ("{0,5} {1}", dataReader ["id"], dataReader ["nombre"]); 
+			dataReader.Close ();
+		}
+
+		private const int ER_DUP_ENTRY = 1062;
+		private static string getUserMessage(MySqlException ex) {
+			switch (ex.Number) {
+				case ER_DUP_ENTRY:
+				return "Dato duplicado. Ese dato ya existe.";
+			}
+			return ex.Message;
 		}
 
 		private static void addParameter(IDbCommand dbCommand, string name, object value) {
