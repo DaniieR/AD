91ad22a0ee24050ceec4401a442ac3b8301cedaf
DaniieR <dam.dromero@institutoserpis.org>
2016-09-29 10:24:02 +0200

    Creado Columnas PArticulo

--- PArticulo/PArticulo/MainWindow.cs
+++ PArticulo/PArticulo/MainWindow.cs
@@ -6,6 +6,12 @@ public partial class MainWindow: Gtk.Window
 	public MainWindow (): base (Gtk.WindowType.Toplevel)
 	{
 		Build ();
+		treeView.AppendColumn ("id", new CellRendererText (), "text", 0);
+		treeView.AppendColumn ("nombre", new CellRendererText (), "text", 0);
+		ListStore listStore = new ListStore (typeof(long), typeof(string));
+		treeView.Model = listStore;
+		listStore.AppendValues (1L, "categoria 1");
+
 	}
 
 	protected void OnDeleteEvent (object sender, DeleteEventArgs a)
