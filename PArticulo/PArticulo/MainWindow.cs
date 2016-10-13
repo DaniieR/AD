0a2b70901f517d8e91dfb2227e5b4add91c489aa
DaniieR <dam.dromero@institutoserpis.org>
2016-10-13 08:24:59 +0200

    Añadido porpiedad Fill

--- PArticulo/PArticulo/MainWindow.cs
+++ PArticulo/PArticulo/MainWindow.cs
@@ -1,5 +1,8 @@
-using System;
 using Gtk;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
 using Org.InstitutoSerpis.Ad;
 
 public partial class MainWindow: Gtk.Window
@@ -7,14 +10,26 @@ public partial class MainWindow: Gtk.Window
 	public MainWindow (): base (Gtk.WindowType.Toplevel)
 	{
 		Build ();
-		string[] columnNames = {"id", "nombre", "precio"};
-		TreeViewHelper.AppendColumns(treeView, columnNames);
-		ListStore listStore = new ListStore (typeof(long), typeof(string), typeof(decimal));
-		//el typeof(decimal) no es reconocido por el Treeview
-		//ListStore listStore = new ListStore (typeof(long), typeof(decimal));
-		treeView.Model = listStore;
-		listStore.AppendValues (1L, "categoria 1", 1.5m);
-		listStore.AppendValues (2L, "categoria 2", 2.5m);
+		IList list = new List<Articulo> ();
+		list.Add (new Articulo(1L, "artículo 1", 1.5m));
+		list.Add (new Articulo(2L, "artículo 2", 2.5m));
+		list.Add (new Articulo(3L, "artículo 3", 3.5m));
+
+		//		IList list = new List<Categoria> ();
+		//		list.Add (new Categoria(1L, "categoría 1"));
+		//		list.Add (new Categoria(2L, "categoría 2"));
+		//		list.Add (new Categoria(3L, "categoría 3"));
+
+		TreeViewHelper.Fill (treeView, list);
+		list.Add (new Articulo(4L, "artículo 4", 4.5m));
+		TreeViewHelper.Fill (treeView, list);
+
+		//		TreeViewHelper.AppendColumns (treeView, typeof(Articulo));
+		//		TreeViewHelper.AppendColumns (treeView, new string[] { "id", "nombre", "precio" });
+		//		ListStore listStore = new ListStore (typeof(long), typeof(string), typeof(decimal));
+		//		listStore.AppendValues (1L, "artículo 1", 1.5m);
+		//		listStore.AppendValues (2L, "artículo 2", 2.5m);
+		//		treeView.Model = listStore;
 	}
 
 	protected void OnDeleteEvent (object sender, DeleteEventArgs a)
@@ -22,4 +37,23 @@ public partial class MainWindow: Gtk.Window
 		Application.Quit ();
 		a.RetVal = true;
 	}
+}
+
+public class Categoria {
+	public Categoria(long id, string nombre) {
+		Id = id;
+		Nombre = nombre;
+	}
+	public long Id { get; set; }
+	public string Nombre { get; set; }
+}
+public class Articulo {
+	public Articulo (long id, string nombre, decimal precio) {
+		Id = id;
+		Nombre = nombre;
+		Precio = precio;
+	}
+	public long Id { get; set; }
+	public string Nombre { get; set; }
+	public decimal Precio { get; set; }
 }
