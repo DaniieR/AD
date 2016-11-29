6bc45f8e9bf54878ffa97e0f05836a58b0305517
DaniieR <dam.dromero@institutoserpis.org>
2016-11-29 10:25:00 +0100

    Hola mundo desde Gradle

---
+++ GHolaMundo/src/main/java/org/institutoserpis/ad/HolaMundo.java
@@ -0,0 +1,11 @@
+package org.institutoserpis.ad;
+
+public class HolaMundo {
+
+	public static void main (String[]args){
+		
+		System.out.println("Hola mundo desde eclipse con gradle");
+		
+	}
+	
+}
