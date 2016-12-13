78fb8e42c8e360bbd2f2d1d981ed6630b3a8248b
DaniieR <dam.dromero@institutoserpis.org>
2016-12-13 10:15:29 +0100

    Comienzo de proyecto GArticulo

--- GMySql/src/main/java/org/institutoserpis/org/ArticuloDAO.java
+++ GMySql/src/main/java/org/institutoserpis/org/ArticuloDAO.java
@@ -2,6 +2,7 @@ package org.institutoserpis.org;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -11,8 +12,11 @@ public class ArticuloDAO {
 	public static void main(String[] args) throws SQLException {
 	//public static void main(String[] args) {
 		Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/dbprueba", "root", "sistemas");
-		Statement statement = connection.createStatement();
-		ResultSet resultSet = statement.executeQuery("SELECT * FROM articulo");
+		PreparedStatement preparedStatement = connection.prepareStatement("Select * from articulo where id > ?");
+		preparedStatement.setObject(1, Long.parseLong(args[0]));
+		ResultSet resultSet = preparedStatement.executeQuery();
+		//Statement statement = connection.createStatement();
+		//ResultSet resultSet = statement.executeQuery("SELECT * FROM articulo");
 		System.out.printf("%5s %-30s %10s %9s\n", "id", "nombre","precio","categoria" );
 		while (resultSet.next()){
 			System.out.printf("%5s %-30s %10s %9s\n",resultSet.getObject("id"),
@@ -21,7 +25,7 @@ public class ArticuloDAO {
 					resultSet.getObject("categoria"));
 			//System.out.printf("     id=%5d\n", resultSet.getObject("id"));
 		}
-		statement.close();
+		preparedStatement.close();
 		connection.close();
 		System.out.println("fin");
 	}
